39326521f09c3f9ece37b4461146dd689abd90e0
Linsssenn <vhynlinssen@gmail.com>
2022-01-05 14:05:37 -0800

    updated database cofig file

--- wp-config.php
+++ wp-config.php
@@ -59,13 +59,13 @@
 define( 'DB_NAME', 'HjIcaugmKI' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'HjIcaugmKI' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '4kdmD7yuEM' );
+define( 'DB_PASSWORD', 'nMSuJo8Tqp' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'remotemysql.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
